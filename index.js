e942586a1d874fb0d9444c3c3e33e70b9dc4cfc8
emmanuel ronoh <eronoh036@gmail.com>
2024-08-02 13:25:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var username = "andrew";
+var password = 12345;
+let myNumber;
+let isUsername = true;
+let isPassword = false;
+const myUsername = "john";
+const myPassword = 12345;
+let float = 1.23;
+console.log(float);
+console.log(myUsername, myPassword);
+console.log(username, password);
+console.log(myNumber);
+console.log(isUsername, isPassword);
+var username;
+
+let numberOne = Number("5");
+let numberTwo = 5;
+console.log(numberOne == numberTwo);
+console.log(numberOne != numberTwo);
+console.log(numberOne !== numberTwo);
+console.log(numberOne === numberTwo);
+console.log(numberOne !== numbertwo);
+
